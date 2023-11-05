24d6ab0a5b0d829a38c5424c3e936c86fa935e90
Aoife <obriea21@tcd.ie>
2023-11-05 20:40:43 +0000

    increment loops by block size

--- matrix-multiplication/atlas_blocked_matrix_mul.c
+++ matrix-multiplication/atlas_blocked_matrix_mul.c
@@ -48,7 +48,7 @@ int main(void) {
     //loop iterating through each block of the right matrix
     for (int current_block_num_of_right_matrix = 0;
          current_block_num_of_right_matrix < matrixDimension / blockSize;
-         current_block_num_of_right_matrix++) {
+         current_block_num_of_right_matrix+= blockSize) {
 
         //create var to hold a block of the right matrix
         double *block_of_right_matrix;
@@ -57,7 +57,7 @@ int main(void) {
         //build the block of the right matrix
         for (int current_col_in_right_matrix = 0;
              current_col_in_right_matrix < matrixDimension / blockSize;
-             current_col_in_right_matrix++) {
+             current_col_in_right_matrix+= blockSize) {
             block_of_right_matrix[current_col_in_right_matrix] = rightMatrix[current_block_num_of_right_matrix +
                                                                              current_col_in_right_matrix *
                                                                              matrixDimension];
@@ -66,7 +66,7 @@ int main(void) {
         //loop iterating through each block of the left matrix
         for (int current_block_num_of_left_matrix = 0;
              current_block_num_of_left_matrix < matrixDimension / blockSize;
-             current_block_num_of_left_matrix++) {
+             current_block_num_of_left_matrix+= blockSize) {
 
             //create var to hold a block of the left matrix
             double *block_of_left_matrix;
@@ -75,7 +75,7 @@ int main(void) {
             //build the block of the left matrix
             for (int current_row_in_left_matrix = 0;
                  current_row_in_left_matrix < matrixDimension / blockSize;
-                 current_row_in_left_matrix++) {
+                 current_row_in_left_matrix+= blockSize) {
 
                 block_of_left_matrix[current_row_in_left_matrix] = leftMatrix[current_row_in_left_matrix +
                                                                               current_block_num_of_left_matrix *
