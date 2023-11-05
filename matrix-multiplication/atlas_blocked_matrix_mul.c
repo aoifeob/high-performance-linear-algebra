072ff801dafbe3d0876b5176f094a35c55dd2022
Aoife <obriea21@tcd.ie>
2023-11-05 14:22:14 +0000

    atlas_blocked_matrix_mul.c tested against a small simple matrix, outputs correct values

--- matrix-multiplication/atlas_blocked_matrix_mul.c
+++ matrix-multiplication/atlas_blocked_matrix_mul.c
@@ -13,8 +13,10 @@ void initMatrix(int matrixDimension, double matrix[]) {
 }
 
 int main(void) {
-    double *leftMatrix;
-    double *rightMatrix;
+//    double *leftMatrix;
+//    double *rightMatrix;
+    double leftMatrix[4] = {1, 1, 0, 0};
+    double rightMatrix[4] = {1, 0, 1, 0};
     double *resultMatrix;
 
     struct timeval tv1, tv2;
@@ -22,19 +24,24 @@ int main(void) {
     double blockMultiplicationTimeElapsed = 0;
 
     int resultMatrixIndex = 0;
-    int matrixDimension = 3;
+    int matrixDimension = 2;
     int iterations = 10;
 
     unsigned long matrixMemorySize = matrixDimension * matrixDimension * sizeof(double);
 
-    leftMatrix = malloc(matrixMemorySize);
-    rightMatrix = malloc(matrixMemorySize);
+//    leftMatrix = malloc(matrixMemorySize);
+//    rightMatrix = malloc(matrixMemorySize);
     resultMatrix = malloc(matrixMemorySize);
 
-    initMatrix(matrixDimension, leftMatrix);
-    initMatrix(matrixDimension, rightMatrix);
+//    initMatrix(matrixDimension, leftMatrix);
+//    initMatrix(matrixDimension, rightMatrix);
 
-    if (!leftMatrix || !rightMatrix || !resultMatrix) {
+//    if (!leftMatrix || !rightMatrix || !resultMatrix) {
+//        printf("Insufficient memory for matrices of dimension %d.\n", matrixDimension);
+//        exit(-1);
+//    }
+
+    if (!resultMatrix) {
         printf("Insufficient memory for matrices of dimension %d.\n", matrixDimension);
         exit(-1);
     }
@@ -49,8 +56,11 @@ int main(void) {
         block_of_right_matrix = malloc(matrixDimension * sizeof(double));
 
         //build the block of the right matrix
-        for (int current_col_in_right_matrix = 0; current_col_in_right_matrix < matrixDimension; current_col_in_right_matrix++) {
-            block_of_right_matrix[current_col_in_right_matrix] = rightMatrix[current_block_num_of_right_matrix + current_col_in_right_matrix * matrixDimension];
+        for (int current_col_in_right_matrix = 0;
+             current_col_in_right_matrix < matrixDimension; current_col_in_right_matrix++) {
+            block_of_right_matrix[current_col_in_right_matrix] = rightMatrix[current_block_num_of_right_matrix +
+                                                                             current_col_in_right_matrix *
+                                                                             matrixDimension];
         }
 
         //loop iterating through each block of the left matrix
@@ -67,7 +77,9 @@ int main(void) {
                  current_row_in_left_matrix < matrixDimension;
                  current_row_in_left_matrix++) {
 
-                block_of_left_matrix[current_row_in_left_matrix] = leftMatrix[current_row_in_left_matrix + current_block_num_of_left_matrix * matrixDimension];
+                block_of_left_matrix[current_row_in_left_matrix] = leftMatrix[current_row_in_left_matrix +
+                                                                              current_block_num_of_left_matrix *
+                                                                              matrixDimension];
 
             }
 
@@ -87,6 +99,8 @@ int main(void) {
                       &resultMatrix[resultMatrixIndex],
                       1); //stride of C
 
+            printf("Result matrix has value %f at index %d\n", resultMatrix[resultMatrixIndex], resultMatrixIndex);
+
             gettimeofday(&tv2, &tz);
             double timeElapsed = (double) (tv2.tv_sec - tv1.tv_sec) + (double) (tv2.tv_usec - tv1.tv_usec) * 1.e-6;
             blockMultiplicationTimeElapsed += timeElapsed;
@@ -102,8 +116,8 @@ int main(void) {
     printf("\nTotal time taken for simple matrix multiplication on array with %dx%d dimensions: %f.\n\n",
            matrixDimension, matrixDimension, blockMultiplicationTimeElapsed);
 
-    free(leftMatrix);
-    free(rightMatrix);
+//    free(leftMatrix);
+//    free(rightMatrix);
     free(resultMatrix);
 
     return 0;
