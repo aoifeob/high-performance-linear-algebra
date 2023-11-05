b68488a66b3b090c5bc45126b1ed479e89a07fea
Aoife <obriea21@tcd.ie>
2023-11-05 23:30:42 +0000

    testing blocking

--- matrix-multiplication/atlas_blocked_matrix_mul.c
+++ matrix-multiplication/atlas_blocked_matrix_mul.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
-#include <cblas.h>
+//#include <cblas.h>
 
 void initMatrix(int matrixDimension, double matrix[]) {
     for (int i = 0; i < matrixDimension; i++) {
@@ -22,13 +22,13 @@ int main(void) {
     double blockMultiplicationTimeElapsed = 0;
 
     int matrixDimension = 1024; //default value, can be overwritten by user input
-    int blockSize = 1; //default value, can be overwritten by user input
+    int blockSize = 2; //default value, can be overwritten by user input
 
-    printf("Enter matrix dimension n : \n\n");
-    scanf("%d", &matrixDimension);
-
-    printf("Enter matrix block width b : \n\n");
-    scanf("%d", &blockSize);
+//    printf("Enter matrix dimension n : \n\n");
+//    scanf("%d", &matrixDimension);
+//
+//    printf("Enter matrix block width b : \n\n");
+//    scanf("%d", &blockSize);
 
     unsigned long matrixMemorySize = matrixDimension * matrixDimension * sizeof(double);
 
@@ -48,48 +48,71 @@ int main(void) {
     //loop iterating through each block of the right matrix
     for (int current_block_num_of_right_matrix = 0;
          current_block_num_of_right_matrix < matrixDimension / blockSize;
-         current_block_num_of_right_matrix += blockSize) {
+         current_block_num_of_right_matrix++) {
 
         //create var to hold a block of the right matrix to be stored in cache
         double *cached_block_of_right_matrix;
         cached_block_of_right_matrix = malloc(matrixDimension * blockSize * sizeof(double));
 
         //build the block of the right matrix to be stored in cache
-        for (int current_col_in_right_matrix = 0;
-             current_col_in_right_matrix < matrixDimension / blockSize;
-             current_col_in_right_matrix += blockSize) {
-            cached_block_of_right_matrix[current_col_in_right_matrix] = rightMatrix[current_block_num_of_right_matrix +
-                                                                                    current_col_in_right_matrix *
-                                                                                    matrixDimension];
+        for (int current_col_in_right_block = 0;
+             current_col_in_right_block < blockSize;
+             current_col_in_right_block++) {
+            for (int current_col_in_right_matrix = 0;
+                 current_col_in_right_matrix < matrixDimension * blockSize;
+                 current_col_in_right_matrix++) {
+                cached_block_of_right_matrix[matrixDimension * current_col_in_right_block +
+                                             current_col_in_right_matrix] = rightMatrix[
+                        current_block_num_of_right_matrix + current_col_in_right_block +
+                        current_col_in_right_matrix *
+                        matrixDimension];
+            }
+        }
+
+        double *block_of_right_matrix_to_multiply;
+        block_of_right_matrix_to_multiply = malloc(matrixDimension * sizeof(double));
+
+        if (blockSize > 1) {
+            //build the block of the right matrix to use for multiplication
+            for (int current_col_in_right_block = 0;
+                 current_col_in_right_block < matrixDimension;
+                 current_col_in_right_block++) {
+                block_of_right_matrix_to_multiply[current_col_in_right_block] = cached_block_of_right_matrix[
+                        current_block_num_of_right_matrix +
+                        current_col_in_right_block * matrixDimension];
+            }
         }
 
         //loop iterating through each block of the left matrix
         for (int current_block_num_of_left_matrix = 0;
              current_block_num_of_left_matrix < matrixDimension / blockSize;
-             current_block_num_of_left_matrix += blockSize) {
+             current_block_num_of_left_matrix++) {
 
             //create var to hold a block of the left matrix to be stored in cache
             double *cached_block_of_left_matrix;
             cached_block_of_left_matrix = malloc(matrixDimension * blockSize * sizeof(double));
 
             //build the block of the left matrix to be stored in cache
-            for (int current_row_in_left_matrix = 0;
-                 current_row_in_left_matrix < matrixDimension / blockSize;
-                 current_row_in_left_matrix += blockSize) {
+            for (int current_row_in_left_block = 0;
+                 current_row_in_left_block < blockSize;
+                 current_row_in_left_block++) {
+                for (int current_row_in_left_matrix = 0;
+                     current_row_in_left_matrix < matrixDimension * blockSize;
+                     current_row_in_left_matrix++) {
 
-                cached_block_of_left_matrix[current_row_in_left_matrix] = leftMatrix[current_row_in_left_matrix +
-                                                                                     current_block_num_of_left_matrix *
-                                                                                     matrixDimension];
+                    cached_block_of_left_matrix[current_row_in_left_block + matrixDimension +
+                                                current_row_in_left_matrix] = leftMatrix[current_row_in_left_matrix +
+                                                                                         current_block_num_of_left_matrix *
+                                                                                         matrixDimension];
 
+                }
             }
+
+            double *block_of_left_matrix_to_multiply;
+            block_of_left_matrix_to_multiply = malloc(matrixDimension * sizeof(double));
 
             //if the cached block contains more than one row/col, isolate the row and col to be used for this multiplication
             if (blockSize > 1) {
-                double *block_of_left_matrix_to_multiply;
-                double *block_of_right_matrix_to_multiply;
-                block_of_left_matrix_to_multiply = malloc(matrixDimension * sizeof(double));
-                block_of_right_matrix_to_multiply = malloc(matrixDimension * sizeof(double));
-
                 //build the block of the left matrix to use for multiplication
                 for (int current_row_in_left_block = 0;
                      current_row_in_left_block < matrixDimension;
@@ -99,51 +122,39 @@ int main(void) {
                             current_row_in_left_block];
                 }
 
-                //build the block of the right matrix to use for multiplication
-                for (int current_col_in_right_block = 0;
-                     current_col_in_right_block < matrixDimension;
-                     current_col_in_right_block++) {
-                    block_of_right_matrix_to_multiply[current_col_in_right_block] = cached_block_of_right_matrix[
-                            current_block_num_of_right_matrix +
-                            current_col_in_right_block * matrixDimension];
-                }
-
                 gettimeofday(&tv1, &tz);
 
-                ATL_dgemm(CblasNoTrans,
-                          CblasNoTrans,
-                          1, //rows in A, C
-                          1, //cols in B, C
-                          matrixDimension, //cols in A, rows in B
-                          1.0,
-                          block_of_left_matrix_to_multiply,
-                          1, //stride of A
-                          block_of_right_matrix_to_multiply,
-                          1, //stride of B
-                          1.0,
-                          &resultMatrix[resultMatrixIndex],
-                          1); //stride of C
+//                ATL_dgemm(CblasNoTrans,
+//                          CblasNoTrans,
+//                          1, //rows in A, C
+//                          1, //cols in B, C
+//                          matrixDimension, //cols in A, rows in B
+//                          1.0,
+//                          block_of_left_matrix_to_multiply,
+//                          1, //stride of A
+//                          block_of_right_matrix_to_multiply,
+//                          1, //stride of B
+//                          1.0,
+//                          &resultMatrix[resultMatrixIndex],
+//                          1); //stride of C
 
                 gettimeofday(&tv2, &tz);
-
-                free(block_of_left_matrix_to_multiply);
-                free(block_of_right_matrix_to_multiply);
             } else {
                 gettimeofday(&tv1, &tz);
 
-                ATL_dgemm(CblasNoTrans,
-                          CblasNoTrans,
-                          1, //rows in A, C
-                          1, //cols in B, C
-                          matrixDimension, //cols in A, rows in B
-                          1.0,
-                          cached_block_of_left_matrix,
-                          1, //stride of A
-                          cached_block_of_right_matrix,
-                          1, //stride of B
-                          1.0,
-                          &resultMatrix[resultMatrixIndex],
-                          1); //stride of C
+//                ATL_dgemm(CblasNoTrans,
+//                          CblasNoTrans,
+//                          1, //rows in A, C
+//                          1, //cols in B, C
+//                          matrixDimension, //cols in A, rows in B
+//                          1.0,
+//                          cached_block_of_left_matrix,
+//                          1, //stride of A
+//                          cached_block_of_right_matrix,
+//                          1, //stride of B
+//                          1.0,
+//                          &resultMatrix[resultMatrixIndex],
+//                          1); //stride of C
 
                 gettimeofday(&tv2, &tz);
             }
@@ -154,9 +165,11 @@ int main(void) {
             //increment var tracking the index of the result matrix to be calculated
             resultMatrixIndex++;
 
+            free(block_of_left_matrix_to_multiply);
             free(cached_block_of_left_matrix);
         }
 
+        free(block_of_right_matrix_to_multiply);
         free(cached_block_of_right_matrix);
     }
 
--- matrix-multiplication/simple_matrix_mul.c
+++ matrix-multiplication/simple_matrix_mul.c
@@ -1,63 +1,63 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/time.h>
-
-void initMatrix(int matrixDimension, double matrix[]) {
-    for (int i = 0; i < matrixDimension; i++) {
-        for (int j = 0; j < matrixDimension; j++) {
-            double value = rand();
-            matrix[i * matrixDimension + j] = value;
-        }
-    }
-}
-
-void multiply(int matrixDimension, double *firstMatrix, double *secondMatrix, double *resultMatrix) {
-    for (int i = 0; i < matrixDimension; i++) {
-        for (int j = 0; j < matrixDimension; j++) {
-            for (int k = 0; k < matrixDimension; k++) {
-                resultMatrix[i * matrixDimension + j] +=
-                        firstMatrix[i * matrixDimension + k] * secondMatrix[k * matrixDimension + j];
-            }
-        }
-    }
-}
-
-int main(void) {
-    double *firstMatrix;
-    double *secondMatrix;
-    double *resultMatrix;
-    struct timeval tv1, tv2;
-    struct timezone tz;
-    int matrixDimension = 1024; //default value, can be overwritten by user input
-
-    printf("Enter matrix dimension n : \n\n");
-    scanf("%d", &matrixDimension);
-
-    unsigned long matrixMemorySize = matrixDimension * matrixDimension * sizeof(double);
-
-    firstMatrix = malloc(matrixMemorySize);
-    secondMatrix = malloc(matrixMemorySize);
-    resultMatrix = malloc(matrixMemorySize);
-
-    if (!firstMatrix || !secondMatrix || !resultMatrix) {
-        printf("Insufficient memory for matrices of dimension %d.\n", matrixDimension);
-        exit(-1);
-    }
-
-    initMatrix(matrixDimension, firstMatrix);
-    initMatrix(matrixDimension, secondMatrix);
-
-    gettimeofday(&tv1, &tz);
-    multiply(matrixDimension, firstMatrix, secondMatrix, resultMatrix);
-    gettimeofday(&tv2, &tz);
-    double timeElapsed = (double) (tv2.tv_sec - tv1.tv_sec) + (double) (tv2.tv_usec - tv1.tv_usec) * 1.e-6;
-
-    printf("Time taken for simple matrix multiplication on array with %dx%d dimensions: %f.\n\n",
-           matrixDimension, matrixDimension, timeElapsed);
-
-    free(firstMatrix);
-    free(secondMatrix);
-    free(resultMatrix);
-
-    return 0;
-}
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <sys/time.h>
+//
+//void initMatrix(int matrixDimension, double matrix[]) {
+//    for (int i = 0; i < matrixDimension; i++) {
+//        for (int j = 0; j < matrixDimension; j++) {
+//            double value = rand();
+//            matrix[i * matrixDimension + j] = value;
+//        }
+//    }
+//}
+//
+//void multiply(int matrixDimension, double *firstMatrix, double *secondMatrix, double *resultMatrix) {
+//    for (int i = 0; i < matrixDimension; i++) {
+//        for (int j = 0; j < matrixDimension; j++) {
+//            for (int k = 0; k < matrixDimension; k++) {
+//                resultMatrix[i * matrixDimension + j] +=
+//                        firstMatrix[i * matrixDimension + k] * secondMatrix[k * matrixDimension + j];
+//            }
+//        }
+//    }
+//}
+//
+//int main(void) {
+//    double *firstMatrix;
+//    double *secondMatrix;
+//    double *resultMatrix;
+//    struct timeval tv1, tv2;
+//    struct timezone tz;
+//    int matrixDimension = 1024; //default value, can be overwritten by user input
+//
+//    printf("Enter matrix dimension n : \n\n");
+//    scanf("%d", &matrixDimension);
+//
+//    unsigned long matrixMemorySize = matrixDimension * matrixDimension * sizeof(double);
+//
+//    firstMatrix = malloc(matrixMemorySize);
+//    secondMatrix = malloc(matrixMemorySize);
+//    resultMatrix = malloc(matrixMemorySize);
+//
+//    if (!firstMatrix || !secondMatrix || !resultMatrix) {
+//        printf("Insufficient memory for matrices of dimension %d.\n", matrixDimension);
+//        exit(-1);
+//    }
+//
+//    initMatrix(matrixDimension, firstMatrix);
+//    initMatrix(matrixDimension, secondMatrix);
+//
+//    gettimeofday(&tv1, &tz);
+//    multiply(matrixDimension, firstMatrix, secondMatrix, resultMatrix);
+//    gettimeofday(&tv2, &tz);
+//    double timeElapsed = (double) (tv2.tv_sec - tv1.tv_sec) + (double) (tv2.tv_usec - tv1.tv_usec) * 1.e-6;
+//
+//    printf("Time taken for simple matrix multiplication on array with %dx%d dimensions: %f.\n\n",
+//           matrixDimension, matrixDimension, timeElapsed);
+//
+//    free(firstMatrix);
+//    free(secondMatrix);
+//    free(resultMatrix);
+//
+//    return 0;
+//}
