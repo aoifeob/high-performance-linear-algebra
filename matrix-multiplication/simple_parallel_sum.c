95f8b9cb907d2fa3e3d037ab6fd113cadefaf698
Aoife <obriea21@tcd.ie>
2023-11-27 21:08:33 +0000

    add simple open mp sum program

--- matrix-multiplication/simple_parallel_sum.c
+++ matrix-multiplication/simple_parallel_sum.c
@@ -1,38 +1,16 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-typedef struct {
-    int valueToSum;
-    int *totalSum;
-} sum_data;
-
-void *sum(void *arg) {
-    sum_data *sum_data = arg;
-
-    *(sum_data->totalSum) += sum_data->valueToSum;
-}
-
-void parallelSum(int numProcesses, int *totalSum) {
-    sum_data *process_sum_data;
-
-    process_sum_data = malloc(numProcesses * sizeof(sum_data));
-
-    //create processes
-    for (int processes = 0; processes < numProcesses; processes++) {
-        process_sum_data[processes].valueToSum = processes;
-        process_sum_data[processes].totalSum = totalSum;
-
-        //calculate slice
-    }
-
-    free(process_sum_data);
-}
+#include <omp.h>
 
 int main(void) {
-    int numProcesses = 8;
+    int numProcesses = omp_get_num_procs();
     int sumTotal = 0;
 
-    parallelSum(numProcesses, &sumTotal);
+    omp_set_num_threads(numProcesses);
+
+    #pragma omp parallel for reduction(+: sumTotal)
+    for (int i = 0; i < numProcesses; i++) {
+        sumTotal += i;
+    }
 
     printf("Total sum is %d \n", sumTotal);
 
