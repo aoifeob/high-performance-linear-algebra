0560b247a682876a49dbbfe6e691f2d2c9156474
Aoife <obriea21@tcd.ie>
2023-11-30 20:08:51 +0000

    update test parallel sum program

--- matrix-multiplication/simple_parallel_sum.c
+++ matrix-multiplication/simple_parallel_sum.c
@@ -5,33 +5,22 @@ int main(void) {
     int numProcesses = omp_get_num_procs();
     int sumTotal = 0;
 
-    printf("Number of processes is %d \n", numProcesses);
-
     omp_set_num_threads(numProcesses);
-
-    int myInt = 0;
+    printf("Number of threads is %d \n", omp_get_num_threads());
 
 #pragma omp parallel shared(myInt)
     {
 
-        int threadNum = omp_get_thread_num();
+        printf("Thread %d beginning execution\n", omp_get_thread_num());
 
-        printf("Thread %d beginning execution\n", threadNum);
-
-#pragma omp parallel for reduction(+: sumTotal)
-        int innerThreadNum = omp_get_thread_num();
-
+    #pragma omp parallel for reduction(+: sumTotal)
         for (int i = 0; i < numProcesses; i++) {
-                printf("Thread %d incrementing total by %d \n", innerThreadNum, i);
-                sumTotal += i;
-            }
+            sumTotal += i;
+        }
 
         printf("Total sum is %d \n", sumTotal);
-
-#pragma omp critical
-        myInt++;
-        printf("Thread %d incremented myInt to %d\n", threadNum, myInt);
     }
 
     return 0;
 }
+
