78624a91de96a274d94e77381ec504cee2b79fb0
Aoife <obriea21@tcd.ie>
2023-11-30 15:01:38 +0000

    testing additional parallel blocks in sum program

--- matrix-multiplication/simple_parallel_sum.c
+++ matrix-multiplication/simple_parallel_sum.c
@@ -9,12 +9,29 @@ int main(void) {
 
     omp_set_num_threads(numProcesses);
 
-    #pragma omp parallel for reduction(+: sumTotal)
-    for (int i = 0; i < numProcesses; i++) {
-        sumTotal += i;
-    }
+    int myInt = 0;
 
-    printf("Total sum is %d \n", sumTotal);
+#pragma omp parallel shared(myInt)
+    {
+
+        int threadNum = omp_get_thread_num();
+
+        printf("Thread %d beginning execution\n", threadNum);
+
+#pragma omp parallel for reduction(+: sumTotal)
+        int innerThreadNum = omp_get_thread_num();
+
+        for (int i = 0; i < numProcesses; i++) {
+                printf("Thread %d incrementing total by %d \n", innerThreadNum, i);
+                sumTotal += i;
+            }
+
+        printf("Total sum is %d \n", sumTotal);
+
+#pragma omp critical
+        myInt++;
+        printf("Thread %d incremented myInt to %d\n", threadNum, myInt);
+    }
 
     return 0;
 }
