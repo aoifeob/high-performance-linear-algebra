d728cf547c529400bc751aedf3b01b41143f134e
Aoife <obriea21@tcd.ie>
2023-12-09 23:26:18 +0000

    bugfix: incorrect max k value in local multiplication

--- matrix-multiplication/simple_parallel_matrix_mul.c
+++ matrix-multiplication/simple_parallel_matrix_mul.c
@@ -128,7 +128,7 @@ int main(int argc, char **argv) {
     for (int col = 0; col < squareSize; col++) {
         for (int row = 0; row < squareSize; row++) {
             double element = 0;
-            for (int k = 0; k < squareSize; k++) {
+            for (int k = 0; k < matrixDimension; k++) {
                 element += leftRows[row + k * squareSize] * rightCols[k + squareSize * col];
             }
             resultSubMatrix[col * squareSize + row] = element;
